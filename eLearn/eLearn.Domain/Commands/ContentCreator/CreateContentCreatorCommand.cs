698bb5d82edb5724ad8731b6440881a83737b3f3
Alex <alex.penin@grupoica.com>
2018-11-27 23:28:14 +0100

    A pattern test to eliminate the business domain service so all logic resides the entities / associated validators

---
+++ eLearn/eLearn.Domain/Commands/ContentCreator/CreateContentCreatorCommand.cs
@@ -0,0 +1,26 @@
+﻿using eLearn.Domain.Contracts.Repositories;
+using eLearn.Domain.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eLearn.Domain.Validations.Commands.ContentCreator
+{
+    public class CreateContentCreatorCommand 
+    {
+        public readonly IContentCreatorRepository contentCreatorRepository;
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Lastname { get; set; }
+        public PersonalEmail PersonalEmail { get; set; }
+
+        public CreateContentCreatorCommand(IContentCreatorRepository contentCreatorRepository)
+        {
+            this.contentCreatorRepository = contentCreatorRepository;
+        }
+    }
+}
---
+++ eLearn/eLearn.Domain/Validations/ContentCreator/CreateContentCreatorValidation.cs
@@ -0,0 +1,24 @@
+﻿using eLearn.Domain.Validations.Commands.ContentCreator;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eLearn.Domain.Validations.ContentCreator
+{
+    public class CreateContentCreatorValidation : AbstractValidator<CreateContentCreatorCommand>
+    {
+        public CreateContentCreatorValidation()
+        {
+            //Entity validation
+            this.RuleFor(x => x.Name).NotNull();
+            this.RuleFor(x => x.Surname).NotNull();
+            this.RuleFor(x => x.Lastname).NotNull();
+
+            //Context validation
+            //TODO, check email is unique
+        }        
+    }
+}
