dba0ebf7b44d497e3f62cf66eebab9b710b64278
Alex Penin <alex.penin@grupoica.com>
2018-11-30 14:54:40 +0100

    Contextual validation

--- eLearn/eLearn.Domain/Validations/ContentCreator/CreateContentCreatorValidation.cs
+++ eLearn/eLearn.Domain/Validations/ContentCreator/CreateContentCreatorValidation.cs
@@ -1,4 +1,5 @@
-﻿using eLearn.Domain.Validations.Commands.ContentCreator;
+﻿using eLearn.Domain.Contracts.Repositories;
+using eLearn.Domain.Validations.Commands.ContentCreator;
 using FluentValidation;
 using System;
 using System.Collections.Generic;
@@ -10,15 +11,32 @@ namespace eLearn.Domain.Validations.ContentCreator
 {
     public class CreateContentCreatorValidation : AbstractValidator<CreateContentCreatorCommand>
     {
-        public CreateContentCreatorValidation()
+        private readonly IContentCreatorRepository contentCreatorRepository;
+        public CreateContentCreatorValidation(IContentCreatorRepository contentCreatorRepository)
         {
+            this.contentCreatorRepository = contentCreatorRepository;
+
             //Entity validation
             this.RuleFor(x => x.Name).NotNull();
             this.RuleFor(x => x.Surname).NotNull();
             this.RuleFor(x => x.Lastname).NotNull();
 
-            //Context validation
-            //TODO, check email is unique
+            //Context validation         
+            var taskList = new List<Task>();
+            taskList.Add(this.RuleFor(x => x.PersonalEmail.ElectronicAdress).EmailMustBeUnique(this.contentCreatorRepository));
+
+            Task.WhenAll(taskList);
         }        
+    }
+
+    public static class EmailUniquenessValidator
+    {
+        public async static Task<IRuleBuilderOptions<T, string>> EmailMustBeUnique<T>(this IRuleBuilder<T, string> ruleBuilder, IContentCreatorRepository repository)
+        {
+            return ruleBuilder.MustAsync(async (request, email, token) =>
+            {
+                return await repository.CountAsync(c => c.PersonalEmail.ElectronicAdress == email) == 0;
+            });
+        }
     }
 }
